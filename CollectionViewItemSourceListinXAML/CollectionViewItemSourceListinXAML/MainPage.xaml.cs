77a0a73202acdacfd8ab1b75ce79d24ebf0ae28f
David Morrow <quickbasic44@outlook.com>
2022-06-14 19:45:11 -0400

    Add project files.

---
+++ CollectionViewItemSourceListinXAML/CollectionViewItemSourceListinXAML/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using CollectionViewItemSourceObservableCollectioninXAML.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewItemSourceListinXAML
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private string name;
+        private string location;
+        private string imageurl;
+
+        public string Name
+        { 
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Location
+        { 
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ImageUrl 
+        {
+            get
+            {
+                return imageurl;
+            }
+            set
+            {
+                imageurl = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            // for codebehind databinding instead of XAML List<Monkey> binding
+            //BindingContext = Monkey.GetMonkey();
+        }
+    }
+}
---
+++ CollectionViewItemSourceListinXAML/CollectionViewItemSourceListinXAML/Models/Monkey.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Web;
+
+namespace CollectionViewItemSourceObservableCollectioninXAML.Models
+{
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+        public ObservableCollection<Monkey> Monkeys { get; set; }
+        public Monkey()
+        {
+            this.Name = "Blue Monkey";
+            this.Location = "Central and East Africa";
+            this.ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg";
+        }
+        public Monkey(string name, Monkey monkey)
+        {
+            this.Name = monkey.Name;
+            this.Location = monkey.Location;
+            this.ImageUrl = monkey.ImageUrl;
+        }
+        public Monkey(string name, string location, string imageurl)
+        {
+            this.Name = name;
+            this.Location = location;
+            this.ImageUrl = imageurl;
+        }
+
+        public static ObservableCollection<Monkey> GetMonkey()
+        {
+            ObservableCollection<Monkey> Monkeys = new ObservableCollection<Monkey>
+            {
+              new Monkey() { Name = "Baboon", Location = "Africa and Asia", ImageUrl = @"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg" },
+              new Monkey() { Name = "Capuchin Monkey", Location = "Central and South America", ImageUrl = @"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg" },
+              new Monkey() { Name = "Blue Monkey", Location = "Central and East Africa", ImageUrl = @"https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg" }
+            };
+            return Monkeys;
+        }
+    }
+}
